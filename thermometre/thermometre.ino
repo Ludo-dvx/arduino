858c33286e0a55f527562cb76931424e85dd0a9f
Ludo <40889240+Ludo-dvx@users.noreply.github.com>
2023-10-20 15:28:43 +0200

    Update thermometre.ino
    Ajout d'un timer de 3 secondes pour la partie débogage.

--- thermometre/thermometre.ino
+++ thermometre/thermometre.ino
@@ -2,26 +2,30 @@
 #include "DHT.h"
 #include <LiquidCrystal.h>
 
-DHT dht(2, DHT11); //definition de la broche du capteur
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12); // definition des broche du lcd
+// Variables pour le débogage
+unsigned long lastDebugTime = 0;     // Dernier moment où le débogage a été effectué
+unsigned long debugInterval = 3000;  // Intervalle de débogage en millisecondes (3 secondes)
+
+
+DHT dht(2, DHT11);                       //definition de la broche du capteur
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);  // definition des broche du lcd
 
 void setup() {
-  dht.begin(); //initialisation le capteur
-  Serial.begin(9600); // Débit de communication pour la communication série a 9600 bauds.
-  lcd.begin(16, 2); // définit le type d'écran lcd 16 x 2
+  dht.begin();         //initialisation le capteur
+  Serial.begin(9600);  // Débit de communication pour la communication série a 9600 bauds.
+  lcd.begin(16, 2);    // définit le type d'écran lcd 16 x 2
 }
 void loop() {
-  float hum = dht.readHumidity(); //lecture de l'humidité
-  float temp = dht.readTemperature(); // lecture de la temperature
+  float hum = dht.readHumidity();      //lecture de l'humidité
+  float temp = dht.readTemperature();  // lecture de la temperature
 
-  if ( (temp >= 19) && (temp < 21) ) {    //teste si la temperature
+  if ((temp >= 19) && (temp < 21)) {  //teste si la temperature
 
     digitalWrite(5, HIGH);  // Led verte allumée
-    digitalWrite(6, LOW);  //  Led rouge éteinte
-  }
-  else {
+    digitalWrite(6, LOW);   //  Led rouge éteinte
+  } else {
     digitalWrite(6, HIGH);  // Led rouge allumée
-    digitalWrite(5, LOW ); // Led verte éteinte
+    digitalWrite(5, LOW);   // Led verte éteinte
   }
 
   // LCD
@@ -30,12 +34,17 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print("Humidite: " + String(int(hum)) + ("%"));
 
-  // DEBOGAGE
-  Serial.print("Humidité: ");
-  Serial.print(hum);
-  Serial.println("%");
-  Serial.print("Temperature: ");
-  Serial.print(temp);
-  Serial.println("°");
+  // Débogage
+  unsigned long currentTime = millis();
+  if (currentTime - lastDebugTime >= debugInterval) {
+    // Effectuez le débogage toutes les 3 secondes
+    Serial.print("Humidité: ");
+    Serial.print(hum);
+    Serial.println("%");
+    Serial.print("Temperature: ");
+    Serial.print(temp);
+    Serial.println("°");
+    Serial.println("-----------------------------");
+    lastDebugTime = currentTime;  // Met à jour le dernier moment du débogage
+  }
 }
-
